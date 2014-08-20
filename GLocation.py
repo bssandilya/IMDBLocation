70bd69155fcef97dc62376101f53d8bf38b93e55
Sandilya Bhamidipati <sandilya@Piranha.local>
2014-08-20 11:21:15 -0700

    config file stores configurations

--- GLocation.py
+++ GLocation.py
@@ -4,19 +4,22 @@ Get latitude longitude from address using google API
 import requests
 import sys
 from pprint import pprint
+import config
 
 def getlatlong(address):
     '''
     returns lat and long from address
     '''
-    apikey = 'AIzaSyD3ojJrvbxvEikhBiLjMAOpPME1piNzCyM'
+
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    payload = {'key':apikey}
+    payload = {'key':config.CONFIG['apikey']}
     payload['address'] = address
+
     try:
         return requests.get(url, params=payload).json()
     except Exception:
         print sys.exc_info()[0]
+        return None
 
 def test():
     ADD = '175 S San Antonio Rd, Los Altos, CA, USA'
---
+++ config.py
@@ -0,0 +1,1 @@
+CONFIG = {'apikey': 'AIzaSyD3ojJrvbxvEikhBiLjMAOpPME1piNzCyMq'}
